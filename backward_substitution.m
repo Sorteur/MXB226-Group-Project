a72524010d7dd2b34fdf7529d2f65ed89b80d60a
Oliver <Oliver@oliver.canvin-himalayas.nord>
2022-09-21 12:14:22 +1000

    Added functions for forward and backward substitution, and Cholesky factorisation

---
+++ backward_substitution.m
@@ -0,0 +1,43 @@
+function x = backward_substitution(A, b, divide)
+%Backward substitution
+%Returns x for Ax=b for upper triangular system
+
+if nargin < 3
+    divide = true;
+end
+
+n = size(A, 2);
+m = size(b,2);
+
+%initialise output
+x = b;
+
+%loop rows
+for i = n:-1:1
+    %loop columns
+    for j = i+1:n
+        x(i,1:m) = x(i,1:m) - A(i,j) * x(j,1:m);
+    end
+    if divide
+        %divide by diagonal element
+        x(i,1:m) = x(i,1:m) / A(i,i);
+    end
+end
+
+%% Untouched
+% n = size(A, 2);
+% 
+% %initialise output
+% x = b;
+% 
+% %loop rows
+% for i = n:-1:1
+%     %loop columns
+%     for j = i+1:n
+%         x(i) = x(i) - A(i,j) * x(j);
+%     end
+%     %divide by diagonal element
+%     x(i) = x(i) / A(i,i);
+% end
+% end
+end
---
+++ forward_substitution.m
@@ -0,0 +1,27 @@
+function x = forward_substitution(A, b, divide)
+%Forward substitution
+%Returns x for Ax=b for lower triangular system
+
+if nargin < 3
+    divide = true;
+end
+
+n = size(A, 2);
+m = size(b,2);
+
+%initialise output
+x = b;
+
+%loop rows
+for i = 1:n
+    %loop columns
+    for j = 1:i-1
+        x(i,1:m) = x(i,1:m) - A(i,j) * x(j,1:m);
+    end
+    if divide
+        %divide by diagonal element
+        x(i,1:m) = x(i,1:m) / A(i,i);
+    end
+end
+end
+
