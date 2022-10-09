9e9791b23ccce696fb58ba606d74d60a32409761
Nikkissja <Nikkissja@nikkissjas-mbp.modem>
2022-10-09 13:54:10 +1000

    Cholesky factorisation for packed storage - not complete yet

---
+++ cholesky_factorisation_packed.m
@@ -0,0 +1,23 @@
+function A = cholesky_factorisation_packed(A, a)
+% performs Cholesky factorisation on SPD matrix A
+
+n = size(A,1);
+
+% this needs to go in somewhere
+%A(i,j) = a(i+j*(j-1)/2)  %i ≤ j 
+
+for j = 1:n
+    for i = 1:j-1
+        for k = 1:i-1
+            A(i,j) = A(i,j) - A(k,i)*A(k,j);
+        end
+        A(i,j) = A(i,j) / A(i,i);
+        A(j,j) = A(j,j) - A(i,j)^2 ;
+    end
+    A(j,j) = sqrt(A(j,j));
+end
+A = triu(A);
+end
+
+
+
