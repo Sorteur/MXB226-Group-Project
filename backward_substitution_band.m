a434acd4f3b1734289e9e3a4edba6e3f72182512
Sorteur <Tyler7cooL@gmail.com>
2022-10-25 22:29:03 +1000

    Finished Cholesky, started subs

---
+++ backward_substitution_band.m
@@ -0,0 +1,28 @@
+function x = backward_substitution_band(A, b, divide)
+%Backward substitution
+%Returns x for Ax=b for upper triangular system
+
+if nargin < 3
+    divide = true;
+end
+
+n = size(A, 2);
+m = size(b,2);
+
+%initialise output
+x = b;
+
+%loop rows
+for i = n:-1:1
+    %loop columns
+    for j = i+1:n
+        x(i,1:m) = x(i,1:m) - A(i,j) * x(j,1:m);
+    end
+    if divide
+        %divide by diagonal element
+        x(i,1:m) = x(i,1:m) / A(i,i);
+    end
+end
+
+end
+
