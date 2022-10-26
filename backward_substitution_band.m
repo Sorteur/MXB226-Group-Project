0cb647fe6f3b089c5ffe90af73ef93d73f82ece4
Sorteur <Tyler7cooL@gmail.com>
2022-10-26 17:46:09 +1000

    finished solving for x

--- backward_substitution_band.m
+++ backward_substitution_band.m
@@ -1,13 +1,11 @@
 function x = backward_substitution_band(A, b, divide)
 %Backward substitution
-%Returns x for Ax=b for upper triangular system
-
+%Returns x for Ax=b for banded system
 if nargin < 3
     divide = true;
 end
 
-n = size(A, 2);
-m = size(b,2);
+[m,n]= size(A);
 
 %initialise output
 x = b;
@@ -16,13 +14,13 @@ x = b;
 for i = n:-1:1
     %loop columns
     for j = i+1:n
-        x(i,1:m) = x(i,1:m) - A(i,j) * x(j,1:m);
+        if i+(m-j)>0
+            x(i) = x(i) - A(i+(m-j),j) * x(j);
+        end
     end
     if divide
         %divide by diagonal element
-        x(i,1:m) = x(i,1:m) / A(i,i);
+        x(i) = x(i) / A(m,i);
     end
 end
-
 end
-
--- forward_substitution_band.m
+++ forward_substitution_band.m
@@ -1,13 +1,12 @@
 function x = forward_substitution_band(A, b, divide)
 %Forward substitution
-%Returns x for Ax=b for lower triangular system
+%Returns x for Ax=b for banded matrix A
 
 if nargin < 3
     divide = true;
 end
 
-n = size(A, 2);
-m = size(b,2);
+[n,m] = size(A);
 
 %initialise output
 x = b;
@@ -16,14 +15,13 @@ x = b;
 for i = 1:n
     %loop columns
     for j = 1:i-1
-        x(i,1:m) = x(i,1:m) - A(i,j) * x(j,1:m);
+        if j+(m-i) > 0
+            x(i) = x(i) - A(i,j+(m-i)) * x(j);
+        end
     end
     if divide
         %divide by diagonal element
-        x(i,1:m) = x(i,1:m) / A(i,i);
+        x(i) = x(i) / A(i,m);
     end
 end
 end
-
-
-
